98db89033059272abe0a4ee15cf4bc1034493b5f
saivarma-ui <55249290+saivarma-ui@users.noreply.github.com>
2023-01-26 12:58:10 +0530

    Add files via upload

---
+++ game/entries.js
@@ -0,0 +1,58 @@
+/**
+ * Every entry must include a title and filename, the rest are optional.
+ * For simplicity in merges, please add to the bottom of the object.
+ *
+ * interface
+ * {
+ *     title: string // display name
+ *     filename: string // the name of your HTML file
+ *     description?: string // description that will be listed with your entry
+ *     author?: string // your name/tag that will be listed
+ *     github?: string // username on github that will display a link to your profile
+ * }
+ */
+
+const entries = [
+	{
+		title: "Dancing Man",
+		filename: "dancing_man.html",
+		description: "The first submission to the One HTML Page Challenge. It is a simple ASCII man starting the dance from Napoleon Dynamite.",
+		author: "Christopher Powroznik (Metroxe)",
+		github: "Metroxe",
+	},
+	{
+		title: "Ant Colony",
+		filename: "ant_colony.html",
+		description: "Simulation of an ant colony creating a never ending underground colony.",
+		author: "Christopher Powroznik (Metroxe)",
+		github: "Metroxe",
+	},
+	{
+		title: "Strange Insults",
+		filename: "strange_insults.html",
+		description: "An insult generator.",
+		author: "Christopher Powroznik (Metroxe)",
+		github: "Metroxe",
+	},
+	{
+		title: "Building Price Estimate",
+		filename: "building.html",
+		description: "Drag the top right corner on the building. Price increases as building gets bigger.",
+		author: "Richard Hung (Richard1320)",
+		github: "richard1320",
+	},
+	{
+		title: "Fool's Mate",
+		filename: "fools-mate.html",
+		description: "In chess, Fool's Mate, also known as the Two-Move Checkmate, is the checkmate in the fewest possible number of moves from the start of the game. This can be achieved only by Black, who can deliver checkmate on move 2 with the queen. Fool's Mate received its name because it can only occur if White commits an extraordinary blunder. Even among rank beginners, the mate almost never occurs in practice.",
+		author: "Chen Hui Jing (huijing)",
+		github: "huijing",
+	},
+	{
+		title: "Car Game",
+		filename: "mini_car_game.html",
+		description: "simple car racing game, collecting energys on road, speeds up as many energy consumed, slow down if miss energy on road. driving is unlimited",
+		author: "irakli kvirikashvili",
+		github: "zghnachvi",
+	}
+];
